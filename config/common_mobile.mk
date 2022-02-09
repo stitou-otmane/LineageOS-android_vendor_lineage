b31539f05b92354be8b4c6e04b22cadbe7c79a54
Otmane <stitou.otmane@gmail.com>
2022-02-09 19:52:36 +0100

    Remove undesired apps

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -18,17 +18,11 @@ PRODUCT_PACKAGES += \
 
 # AOSP packages
 PRODUCT_PACKAGES += \
-    Email \
-    ExactCalculator \
     Exchange2
 
 # Lineage packages
 PRODUCT_PACKAGES += \
-    AudioFX \
     Backgrounds \
-    Eleven \
-    Etar \
-    Jelly \
     LockClock \
     Profiles \
     Seedvault \
